7905159883d0562c47aa785f1c0d060631ab30c9
matteopey <matteo.pey@hotmail.it>
2018-10-06 15:39:47 +0200

    Liqui: useless using statement

--- ExchangesApi/Exchanges/LiquiApi/PublicMethods.cs
+++ ExchangesApi/Exchanges/LiquiApi/PublicMethods.cs
@@ -6,7 +6,6 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using ExchangesApi.Exchanges.BinanceApi.ApiCalls;
 using ExchangesApi.Exchanges.LiquiApi.Data;
 using Newtonsoft.Json;
 
