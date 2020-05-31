ab22ae73df4dc46f1fcc6abdcf47d049764ffa7b
Matteo Peyronel <28830727+matteopey@users.noreply.github.com>
2020-05-31 16:10:23 +0200

    Update Binance API version to 3

--- ExchangesApi/Exchanges/BinanceApi/Constants.cs
+++ ExchangesApi/Exchanges/BinanceApi/Constants.cs
@@ -3,6 +3,6 @@ namespace ExchangesApi.Exchanges.BinanceApi
     class Constants
     {
         public static string Endpoint => "https://api.binance.com/";
-        public static string AbsolutePath => "api/v1/";
+        public static string AbsolutePath => "api/v3/";
     }
 }
