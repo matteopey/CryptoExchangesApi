f732be67e8f87a0564f1231a38870687a9fd5fb0
matteopey <28830727+matteopey@users.noreply.github.com>
2020-06-15 10:55:16 +0200

    Update bittrex api endpoint

--- ExchangesApi/Exchanges/BittrexApi/Constants.cs
+++ ExchangesApi/Exchanges/BittrexApi/Constants.cs
@@ -2,7 +2,7 @@ namespace ExchangesApi.Exchanges.BittrexApi
 {
     class Constants
     {
-        public static string Endpoint => "https://bittrex.com/";
+        public static string Endpoint => "https://api.bittrex.com/";
         public static string PathPublic => "api/v1.1/public/";
     }
 }
